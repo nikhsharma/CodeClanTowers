8c03ed371ff8900dd2ef68966f587b2354e87156
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-04 15:06:33 +0100

    Create guest class.

---
+++ src/test/java/GuestTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuestTest {
+    Guest guest;
+
+    @Before
+    public void before() {
+        guest = new Guest("Bob");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Bob", guest.getName());
+    }
+}
